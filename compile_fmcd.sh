bc080578eb6d95fb3d5ad8dd39b2b10bf7ccd7b7
Aymeric Spiga <aymeric.spiga@lmd.jussieu.fr>
2015-02-14 09:33:33 +0100

    renamed compile command and merged old and new

--- compile_fmcd.sh
+++ compile_fmcd.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+######################################################################################
 ###
-### fmcd_gfortran.sh
+### compile_gfortran.sh
+###
+### - requirements: f2py tool + netCDF librairies compiled in Fortran
 ###
 ### ---> This should be used to make MCD fortran stuff directly accessible in python 
 ### ---> This script is for gfortran, but it is easy to adapt to your other compilers
@@ -10,14 +13,28 @@
 ###
 ### AS. 17/04/2012. 
 ###
+######################################################################################
 
-### LINKS
-NETCDF=/home/marshttp/NETCDF/netcdf64-4.0.1_gfortran_fPIC/
-wheremcd=/home/marshttp/MCD_v4.3//mcd/
+## server use -- version 5
+#NETCDF=/home/marshttp/NETCDF/netcdf64-4.0.1_gfortran_fPIC/
+#wheremcd=/home/marshttp/MCD_v5.1//mcd/
+
+## local use -- version 5
+NETCDF=/home/aymeric/Science/NETCDF/netcdf64-4.0.1_gfortran
+wheremcd=./mcd
+num="5"
+
+## local use -- version 4
+wheremcd=../MCD_v4.3/mcd/
+num=""
+
+######################################################################################
+######################################################################################
+######################################################################################
 
 ### LOG FILE
-touch fmcd.log
-\rm fmcd.log
+touch fmcd$num.log
+\rm fmcd$num.log
 
 ### COPY/PREPARE SOURCES
 ### perform changes that makes f2py not to fail
@@ -27,12 +44,12 @@ sed s/"\!\!'"/"'"/g $wheremcd/heights.F          | sed s/"\!'"/"'"/g | sed s/"\!
 sed s/"\!\!'"/"'"/g $wheremcd/constants_mcd.inc  | sed s/"\!'"/"'"/g | sed s/"\!"/"\n\!"/g > constants_mcd.inc
 
 ### BUILD THROUGH f2py WHAT IS NECESSARY TO CREATE THE PYTHON FUNCTIONS
-touch fmcd.pyf
-\rm fmcd.pyf
-f2py -h fmcd.pyf -m fmcd tmp.call_mcd.F tmp.julian.F tmp.heights.F > fmcd.log 2>&1
+touch fmcd$num.pyf
+\rm fmcd$num.pyf
+f2py -h fmcd$num.pyf -m fmcd$num tmp.call_mcd.F tmp.julian.F tmp.heights.F > fmcd$num.log 2>&1
 
 ### IMPORTANT: we teach f2py about variables in the call_mcd subroutines which are intended to be out
-sed s/"real :: pres"/"real, intent(out) :: pres"/g fmcd.pyf | \
+sed s/"real :: pres"/"real, intent(out) :: pres"/g fmcd$num.pyf | \
 sed s/"real :: dens"/"real, intent(out) :: dens"/g | \
 sed s/"real :: temp"/"real, intent(out) :: temp"/g | \
 sed s/"real :: zonwind"/"real, intent(out) :: zonwind"/g | \
@@ -40,18 +57,17 @@ sed s/"real :: merwind"/"real, intent(out) :: merwind"/g | \
 sed s/"real dimension(5) :: meanvar"/"real dimension(5),intent(out) :: meanvar"/g | \
 sed s/"real dimension(100) :: extvar"/"real dimension(100),intent(out) :: extvar"/g | \
 sed s/"real :: seedout"/"real, intent(out) :: seedout"/g | \
-sed s/"integer :: ier"/"integer, intent(out) :: ier"/g > fmcd.pyf.modif
-mv fmcd.pyf.modif fmcd.pyf
+sed s/"integer :: ier"/"integer, intent(out) :: ier"/g > fmcd$num.pyf.modif
+mv fmcd$num.pyf.modif fmcd$num.pyf
 
 ### BUILD
-f2py -c fmcd.pyf tmp.call_mcd.F tmp.julian.F tmp.heights.F --fcompiler=gnu95 \
+f2py -c fmcd$num.pyf tmp.call_mcd.F tmp.julian.F tmp.heights.F --fcompiler=gnu95 \
   -L$NETCDF/lib -lnetcdf \
   -lm -I$NETCDF/include \
   --f90flags="-fPIC" \
   --f77flags="-fPIC" \
   --verbose \
-  > fmcd.log 2>&1
-#  --include-paths $NETCDF/include:$NETCDF/lib \ ---> makes it fail
+  > fmcd$num.log 2>&1
 
 ### CLEAN THE PLACE
 \rm tmp.call_mcd.F tmp.julian.F tmp.heights.F constants_mcd.inc
