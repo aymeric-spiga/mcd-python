855df37fa608a9a70e00a1c57678b7d636ce6de2
Aymeric Spiga <aymeric.spiga@upmc.fr>
2018-07-06 15:22:38 +0200

    bug fix misplaced havetop if instance

--- mcd.py
+++ mcd.py
@@ -1034,17 +1034,17 @@ class mcd():
         palette = get_cmap(name=self.colorm)
 
         ## topography contours
-        rcParams['contour.negative_linestyle'] = 'solid' # negative contours solid instead of dashed
-        zelevc = np.linspace(-9.,20.,11.,0.)
-        if isproj:
-           [xc2,yc2] = np.meshgrid(xc,yc)
-           xc3,yc3 = yeah(xc2,yc2)
-           yeah.contour( xc3, yc3, fieldc, zelevc, colors='black',linewidths = 0.4 )
-           [xc2,yc2] = np.meshgrid(np.array(xc)-360.,yc)
-           xc3,yc3 = yeah(xc2,yc2)
-           yeah.contour( xc3, yc3, fieldc, zelevc, colors='black',linewidths = 0.4 )
-        else:
-           if havetopo:
+        if havetopo:
+          rcParams['contour.negative_linestyle'] = 'solid' # negative contours solid instead of dashed
+          zelevc = np.linspace(-9.,20.,11.,0.)
+          if isproj:
+             [xc2,yc2] = np.meshgrid(xc,yc)
+             xc3,yc3 = yeah(xc2,yc2)
+             yeah.contour( xc3, yc3, fieldc, zelevc, colors='black',linewidths = 0.4 )
+             [xc2,yc2] = np.meshgrid(np.array(xc)-360.,yc)
+             xc3,yc3 = yeah(xc2,yc2)
+             yeah.contour( xc3, yc3, fieldc, zelevc, colors='black',linewidths = 0.4 )
+          else:
              yeah.contour( np.array(xc)       , yc, fieldc, zelevc, colors='black',linewidths = 0.4)
              yeah.contour( np.array(xc) - 360., yc, fieldc, zelevc, colors='black',linewidths = 0.4)
 
